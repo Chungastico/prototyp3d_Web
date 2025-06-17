9285e3d182e86ab1900d1bc13dd909146a0408d8
Chungastico <salamancaaleander03@gmail.com>
2025-06-16 23:38:47 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+        colors: {
+            beige: {
+            claro: '#E8DFD5',
+            medio: '#DEB399',
+            },
+            naranja: '#FF7123',
+            azul: {
+            DEFAULT: '#3B4883',
+            oscuro: '#262C4D',
+            },
+            negro: '#202124',
+        },
+        fontFamily: {
+            sans: ['"Poppins"', 'sans-serif'],
+        },
+        },
+    },
+    plugins: [],
+}
